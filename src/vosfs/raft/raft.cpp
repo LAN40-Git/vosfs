465daf36db93f29973991744c1c15895d008f0ae
LAN40 <sdfk32@outlook.com>
2026-04-02 18:00:52 +0800

    2026.4.2 18:00

--- src/vosfs/raft/raft.cpp
+++ src/vosfs/raft/raft.cpp
@@ -1,5 +1,12 @@
 #include "vosfs/raft/raft.hpp"
 
 auto vosfs::raft::RaftNode::start_election_timeout() -> kosio::async::Task<void> {
+    kosio::util::FastRand rand;
+    while (!is_shutdown_.load(std::memory_order_relaxed)) {
+        // 150-300ms election timeout
+        auto election_timeout = rand.rand_range(150, 300);
+        co_await kosio::time::sleep(election_timeout);
 
+
+    }
 }
