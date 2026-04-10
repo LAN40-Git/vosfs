d3cfe82704ab1e190dcceaffc88a34fa10872de9
LAN40 <sdfk32@outlook.com>
2026-04-10 18:00:47 +0800

    2026.4.10 18:00

--- include/vosfs/raft/internal/state_machine.hpp
+++ include/vosfs/raft/internal/state_machine.hpp
@@ -1,10 +1,23 @@
 #pragma once
+#include <span>
+#include <coroutine>
+#include <cstdint>
+#include <unordered_map>
+#include "vosfs/api/serverpb/raft.pb.h"
+#include "vosfs/rpc/result.hpp"
 
 namespace vosfs::raft::detail {
+struct ResponseContext {
+    std::coroutine_handle<> handle;
+    std::span<char>         resp_payload;
+    rpc::RpcResult          result;
+};
+
 class StateMachine {
 public:
+    void apply(std::span<const LogEntry> entries);
 
 private:
-
+    std::unordered_map<uint64_t, ResponseContext> pending_map_;
 };
 } // namespace vosfs::raft::detail
--- src/vosfs/raft/internal/state_machine.cpp
+++ src/vosfs/raft/internal/state_machine.cpp
@@ -1,1 +1,21 @@
 #include "vosfs/raft/internal/state_machine.hpp"
+#include <kosio/common/debug.hpp>
+#include <kosio/runtime/runtime.hpp>
+
+void vosfs::raft::detail::StateMachine::apply(std::span<const LogEntry> entries) {
+    for (const auto& entry : entries) {
+        auto index = entry.index();
+        ClientCommand command;
+        if (!command.ParseFromString(entry.command())) {
+            LOG_WARN("Failed to parse command at entry {}", index);
+            auto it = pending_map_.find(index);
+            if (it != pending_map_.end()) {
+                auto& context = it->second;
+                context.result = rpc::make_result(rpc::RpcResult::kMessageParseFailed);
+            }
+            continue;
+        }
+
+
+    }
+}
